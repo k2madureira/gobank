b08c84507ae39b62b212d5186a4614f37e4c24bb
k2madureira <lenilsonmadureira@gmail.com>
2024-07-02 12:17:02 -0300

    feat:adicionado env e configs

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -6,19 +6,20 @@ import (
 	"os"
 	"testing"
 
+	"github.com/k2madureira/gobank/util"
 	_ "github.com/lib/pq"
 )
-
-const dbDriver = "postgres"
-const dbSource = "postgres://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
 
 var testQueries *Queries
 var testDB *sql.DB
 
 func TestMain(m *testing.M) {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
 
-	var err error
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBTestSource)
 	if err != nil {
 		log.Fatal("caccot connect to db:", err)
 	}
--- main.go
+++ main.go
@@ -6,16 +6,16 @@ import (
 
 	"github.com/k2madureira/gobank/api"
 	dbbank "github.com/k2madureira/gobank/db/sqlc"
+	"github.com/k2madureira/gobank/util"
 	_ "github.com/lib/pq"
 )
 
-const dbDriver = "postgres"
-const dbSource = "postgres://postgres:postgres@localhost:5432/simple_bank?sslmode=disable"
-const serverAddress = "0.0.0.0:3000"
-
 func main() {
-
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("caccot connect to db:", err)
 	}
@@ -23,7 +23,7 @@ func main() {
 	store := dbbank.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
